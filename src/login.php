c03e6e7e065539da18955ae4413ffc8deee0277e
KillianDudduss <Killian.dusautiez@laposte.net>
2016-10-24 16:35:29 +0200

    Ajout fonction IsConnected
    Ajout de la fonction est connecté qui renvoie le nom des utilisateurs actuellement connecté au chat.

--- src/login.php
+++ src/login.php
@@ -105,4 +105,23 @@ Function logon($username,$password,$confirmpass)
 		}
 	}
 }
+
+Function IsConnected ()
+{
+	$filename='./../db/online.txt'
+	$file=fopen($filename,'r');
+	$filecontents=fread($file,filesize($filename));
+	fclose($file);
+	
+	$array = new array ();
+	
+	$lines=explode("/n", $filecontents);
+	foreach ($lines as $line) 
+	{
+		$array += $line;
+	}
+	return $array;
+}
+
+	
 ?>
