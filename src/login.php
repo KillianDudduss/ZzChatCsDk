281a1eda91f0e88d7287e31c634d4e07ee6c2016
sichaizze <sichaizze@gmail.com>
2016-10-24 15:13:40 +0200

    fonction logon

--- src/login.php
+++ src/login.php
@@ -7,7 +7,7 @@ Function matchlog($login,$pass)
 
 	if (isset($login)&&isset($pass))
 	{
-		$filename='./../db/user.txt';
+		$filename='./../db/users.txt';
 		$file=fopen($filename, 'r');
 		$filecontents = fread($file, filesize($filename));
 		$fclose($file);
@@ -81,5 +81,28 @@ Function disconnect($username)
 	}
 	fclose($file);
 	$auth=false;
+}
+
+Function logon($username,$password,$confirmpass)
+{
+	if (isset($username)&&isset($password)&&(isser($confirmpass)))
+	{
+		if ($password==$confirmpass)
+		{
+			$filename='./../db/users.txt';
+			$file=fopen($filename, 'r');
+			$filecontents = fread($file, filesize($filename));
+			$fclose($file);
+			$file=fopen($filename, 'w');
+			fwrite($file,$filecontents);
+			fwrite($file,$username+";;"+$password+"/n");
+			fclose($file);
+			connect($username,$password);
+		}
+		else
+		{
+			header('Location: index.html/#subscribe');
+		}
+	}
 }
 ?>
