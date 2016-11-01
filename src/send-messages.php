ff022db1b53cbacb7225bb2042680ecc62e0851e
KillianDudduss <Killian.dusautiez@laposte.net>
2016-11-01 18:38:32 +0100

    edit changement de fonction

--- src/send-messages.php
+++ src/send-messages.php
@@ -1,21 +1,13 @@
 <php?
 
-Function got-messages($username)
+Function send-messages()
 {
-	if (isset($_POST['envoi_message']))
-	{
-		$message = $username + ' écrit : ' + $_POST['message'];
-		
-		$filename='./../db/messages.txt';
-		$file=fopen($filename,'r');
-		$filecontent=fread($file,filesize($filename));
-		fclose($file);
+	$filename='./../db/messages.txt';
+	$file=fopen($filename,'r');
+	$filecontent=fread($file,filesize($filename));
+	fclose($file);
 
-		$file=fopen($filename,'w');
-		fwrite($file, $filecontent + $message +'/n');
-		fclose($file);
-	}	
+	return $filecontent;
 }
-
 
 ?>
