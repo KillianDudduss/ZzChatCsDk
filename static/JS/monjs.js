b8a7ec2d0537e9ef25fea72d7dcfab193939a4c3
sichaizze <sichaizze@gmail.com>
2016-11-07 15:18:04 +0100

    modif js

--- static/JS/monjs.js
+++ static/JS/monjs.js
@@ -1,3 +1,12 @@
+$('input[type=botton]').on('click',function(){
+  var cursorPos=$('#message-send').prop('selectionStart');
+  var v = $('#message-send').val(); 
+  var textBefore = v.substring(0,cursorPos);
+  var textAfter = v.substring(cursorPos,v.length);
+  $('#message-send').val(textBefore+$(this).val()+textAfter);
+})
+
+
 function BasculeElement(_this){
       var Onglet_li = document.getElementsByTagName('LI');
       for(var i = 0; i < Onglet_li.length; i++){
