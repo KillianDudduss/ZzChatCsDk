87247d113c3208705a984efe4d50afb21b1141d7
KillianDudduss <Killian.dusautiez@laposte.net>
2016-12-08 20:25:29 +0100

    Update logon.php

--- src/logon.php
+++ src/logon.php
@@ -7,10 +7,14 @@ if (!isset($_SESSION['username']))
 } 
 logon($_POST['username'],$_POST['password'],$_POST['confirmpass'],$_POST['email']);
 
+
+//Function to sign up.
+
 function logon($username,$password,$confirmpass,$email)
 {
 	if (isset($username)&&isset($password)&&isset($confirmpass)&&isset($email))
 	{
+		//we need to check if the pass = confirmpass
 		if ($password==$confirmpass)
 		{
 			$filename='./../db/users.txt';
@@ -32,6 +36,7 @@ function logon($username,$password,$confirmpass,$email)
 			}
 			if ($use==0)
 			{
+				//adding the user into the users.txt
 				$file=fopen($filename, 'w');
 				fwrite($file,$filecontents);
 				fwrite($file,$username.";;".sha1($password).";;".$email."\r\n");
@@ -40,7 +45,9 @@ function logon($username,$password,$confirmpass,$email)
 			}
 			else
 			{
+				//error message if the user already exist
 				echo "le nom d'utilisateur est déja utilisé, veuillez vous connectez ou en choisir un autre.";
+				// We need to echo it into the correct language too...
 			}
 		}
 		else
