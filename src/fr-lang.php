cff8404bc02624a0667038253b54faac3eb1c6e4
KillianDudduss <Killian.dusautiez@laposte.net>
2016-12-08 19:07:27 +0100

    Update fr-lang.php

--- src/fr-lang.php
+++ src/fr-lang.php
@@ -1,7 +1,14 @@
 <?php
 
 $TXT_1= "Se Connecter"
-
+$TXT_2= "S'inscrire"
+$TXT_3= "Contacter" 
+$TXT_4= "Identifiant"
+$TXT_5= "Mot de Passe"
+$TXT_6= "Se souvenir de moi"
+$TXT_7= "Se connecter"
+$TXT_8= "Mot de passe oublié?"
+$TXT_9= "  "
 
 
 
