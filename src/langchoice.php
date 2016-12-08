08db2f00d647c84c730c6a287de4a160c80b8994
KillianDudduss <Killian.dusautiez@laposte.net>
2016-12-08 18:59:31 +0100

    Update langchoice.php

--- src/langchoice.php
+++ src/langchoice.php
@@ -4,13 +4,13 @@
 // we include the file fr-lang.php
 if ($_GET['langue'] == "fr" || $langue == "fr") 
 	{
-		include('lang/fr-lang.php');
+		include('src/fr-lang.php');
 	}
 // Use of the good language
 // if the cookie is 'in' or if the request is 'in',
 // we include the file fr-lang.php
 elseif ($_GET['langue'] == "en" || $langue == "en")
 	{
-		include('lang/en-lang.php');
+		include('src/en-lang.php');
 	} 
 ?>
