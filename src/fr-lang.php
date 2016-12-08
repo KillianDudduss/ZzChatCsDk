efa840eb6c8449c3423b1d8df6090fab62e2dc02
KillianDudduss <Killian.dusautiez@laposte.net>
2016-12-08 19:18:52 +0100

    Update fr-lang.php

--- src/fr-lang.php
+++ src/fr-lang.php
@@ -8,10 +8,16 @@ $TXT_5= "Mot de Passe"
 $TXT_6= "Se souvenir de moi"
 $TXT_7= "Se connecter"
 $TXT_8= "Mot de passe oublié?"
-$TXT_9= "  "
-
-
-
+$TXT_9= "Adresse Email"
+$TXT_10= "Confirmer Mot de Passe" 
+$TXT_11= "S'enregistrer maintenant"
+$TXT_12= "Se déconnecter" 
+$TXT_13= "Paramètres"
+$TXT_14= "Gras" 
+$TXT_15= "Lien"
+$TXT_16= "paramètre du chat" 
+$TXT_17= "Utilisateurs connectés"
+$TXT_18= "Envoyer" 
 
 
 ?>
