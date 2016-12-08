9b1b6c3674f42f4d477a81de7d1ddb0ce6520f66
KillianDudduss <Killian.dusautiez@laposte.net>
2016-12-08 19:09:10 +0100

    Create en-lang.php

---
+++ src/en-lang.php
@@ -0,0 +1,11 @@
+<?php
+$ TXT_1 = "Sign In"
+$ TXT_2 = "Sign up"
+$ TXT_3 = "Contact"
+$ TXT_4 = "Id"
+$ TXT_5 = "Password"
+$ TXT_6 = "Remember me"
+$ TXT_7 = "Connect"
+$ TXT_8 = "Password forgotten?"
+$ TXT_9 = ""
+?>
